9a79b339f78c58a747ff3dd4d12a7987c32e35c1
Gaetan Thereau <gaetan.thereau@gmail.com>
2021-01-16 16:39:24 +0100

    Connect to MongoDB database

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://User1:D57BZ36m77Wia3p@cluster0.xlqdt.mongodb.net/sopekocko?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
@@ -15,12 +25,39 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(bodyParser.json());
+
+//validate login (get login) with test login only
+app.use("/api/auth/login", (req, res, next) => {
+  const user = {
+    email: "test@test.com",
+    password: "test",
+  };
+  res.status(200).json(user);
+});
+
+//get sauces
 app.use("/api/sauces", (req, res, next) => {
   const sauces = [
     {
-      id: "ObjectID — identifiant unique créé par MongoDB",
+      id: "123456789",
       userId: "string",
       name: "tabasco",
+      manufacturer: "tabasco corp",
+      description: "wow",
+      mainPepper: "hot stuff made by mother nature",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519666213631-be6e024eac6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1888&q=80",
+      heat: 5,
+      likes: 3,
+      dislikes: 1,
+      usersLiked: ["me", "mom", "sister"],
+      usersDisliked: ["brother"],
+    },
+    {
+      id: "987654321",
+      userId: "string",
+      name: "tabasco2",
       manufacturer: "tabasco corp",
       description: "wow",
       mainPepper: "hot stuff made by mother nature",
@@ -35,24 +72,5 @@ app.use("/api/sauces", (req, res, next) => {
   ];
   res.status(200).json(sauces);
 });
-
-// app.use((req, res, next) => {
-//   console.log("requete recue");
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   res.status(201);
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   res.json({ message: "Votre requête a bien été reçue !" });
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log("reponse envoyee avec succes");
-// });
 
 module.exports = app;
