9c055448e2db4202b6c509270477af01227a6c49
Gaetan Thereau <gaetan.thereau@gmail.com>
2021-01-16 18:47:00 +0100

    Create router structure for the sauces

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const saucesRoutes = require("./routes/sauces");
+
 const mongoose = require("mongoose");
 mongoose
   .connect(
@@ -12,6 +14,7 @@ mongoose
 
 const app = express();
 
+//CORS:
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -27,50 +30,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-//validate login (get login) with test login only
-app.use("/api/auth/login", (req, res, next) => {
-  const user = {
-    email: "test@test.com",
-    password: "test",
-  };
-  res.status(200).json(user);
-});
-
-//get sauces
-app.use("/api/sauces", (req, res, next) => {
-  const sauces = [
-    {
-      id: "123456789",
-      userId: "string",
-      name: "tabasco",
-      manufacturer: "tabasco corp",
-      description: "wow",
-      mainPepper: "hot stuff made by mother nature",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519666213631-be6e024eac6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1888&q=80",
-      heat: 5,
-      likes: 3,
-      dislikes: 1,
-      usersLiked: ["me", "mom", "sister"],
-      usersDisliked: ["brother"],
-    },
-    {
-      id: "987654321",
-      userId: "string",
-      name: "tabasco2",
-      manufacturer: "tabasco corp",
-      description: "wow",
-      mainPepper: "hot stuff made by mother nature",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519666213631-be6e024eac6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1888&q=80",
-      heat: 5,
-      likes: 3,
-      dislikes: 1,
-      usersLiked: ["me", "mom", "sister"],
-      usersDisliked: ["brother"],
-    },
-  ];
-  res.status(200).json(sauces);
-});
+app.use("/api", saucesRoutes);
 
 module.exports = app;
---
+++ models/sauces.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: {
+    type: Number,
+    required: true,
+    min: [1, "minimum is 1"],
+    max: [10, "maximum is 10"],
+  },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: [{ type: String, required: true }],
+  usersDisliked: [{ type: String, required: true }],
+});
+
+module.exports = mongoose.model("Sauce", sauceSchema);
