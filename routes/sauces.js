51d41d783d93129fcefbe20d1276a788a3c71fb5
Gaetan Thereau <gaetan.thereau@gmail.com>
2021-01-16 22:24:49 +0100

    Build controller getOneSauce

--- routes/sauces.js
+++ routes/sauces.js
@@ -8,5 +8,6 @@ const saucesCtrl = require("../controllers/sauces");
 
 router.get("/sauces", auth, saucesCtrl.getSauces);
 router.post("/sauces", auth, multer, saucesCtrl.createSauce);
+router.get("/sauces/:id", auth, saucesCtrl.getOneSauce);
 
 module.exports = router;
